cea98975589eb42fabf9f1ea2cdf62c3af39e0e2
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-08 04:31:35 +0700

    Variables

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+	var x int
+	var y float64 = math.Pi
+	var max, min int
+	var x2 int = 10
+	var c string = "Hello World!"
+	var z float64 = 1.045
+
+	fmt.Println("Hello, Go!", x, y, max, min, x2, c, z)
+
 }
