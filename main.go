6d4bebb9367044120b317d1935f6a20454e11f2d
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-08 05:18:18 +0700

    Print

--- main.go
+++ main.go
@@ -20,4 +20,20 @@ func main() {
 	var symbol int32 = 'c'
 	fmt.Println(string(symbol))
 
+	var name string
+	var age int
+	fmt.Print("Введите имя: ")
+	fmt.Scan(&name)
+	fmt.Print("Введите возраст: ")
+	fmt.Scan(&age)
+
+	fmt.Println(name, age)
+
+	fmt.Print("hello, world")
+	fmt.Print("hello, world")
+	// вывод будет в одну строку:
+	// hello, worldhello, world
+
+	fmt.Println("hello, world")
+	fmt.Print("hello, world")
 }
