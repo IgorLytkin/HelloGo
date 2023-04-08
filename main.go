75418be09d82bc70fb30443db47beaca2b5dc46d
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-09 06:06:00 +0700

    Шаг 1.9 задача 2

--- main.go
+++ main.go
@@ -10,30 +10,19 @@ func main() {
 	if err != nil {
 		return
 	}
-	switch {
-	case n > 10000:
-		{
-			fmt.Print("Число должно быть меньше 10000 !")
-			return
-		}
-	case n == 10000:
-		{
-			fmt.Print(1)
-		}
+	var n1_1, n1_2, n1_3, n2_1, n2_2, n2_3 int
+	n1_1 = n / 100000
+	n1_2 = (n - n1_1*100000) / 10000
+	n1_3 = (n - n1_1*100000 - n1_2*10000) / 1000
 
-	case n >= 1000:
-		{
-			fmt.Print(n / 1000)
-		}
-	case n >= 100:
-		{
-			fmt.Print(n / 100)
-		}
-	case n >= 10:
-		{
-			fmt.Print(n / 10)
-		}
-	default:
-		fmt.Print(n)
+	n2_1 = (n - n1_1*100000 - n1_2*10000 - n1_3*1000) / 100
+	n2_2 = (n - n1_1*100000 - n1_2*10000 - n1_3*1000 - n2_1*100) / 10
+	n2_3 = n - n1_1*100000 - n1_2*10000 - n1_3*1000 - n2_1*100 - n2_2*10
+
+	if n1_1+n1_2+n1_3 == n2_1+n2_2+n2_3 {
+		fmt.Print("YES")
+
+	} else {
+		fmt.Print("NO")
 	}
 }
