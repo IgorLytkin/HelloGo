84220286a19c3980fd836bd71041c5aeb511ee61
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-08 07:57:03 +0700

    Константы

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 8
-	const b int = 10
-	a = a + b
+	const (
+		Sunday    = 0
+		Monday    = 1
+		Tuesday   = 2
+		Wednesday = 3
+		Thursday  = 4
+		Friday    = 5
+		Saturday  = 6
+	)
+	fmt.Println(Sunday)   // вывод 0
+	fmt.Println(Saturday) // вывод 6
 
-	fmt.Println(a)
 }
