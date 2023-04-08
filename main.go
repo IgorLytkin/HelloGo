40774731d2df9f73c7e851c06ae1ff7404a265c9
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-08 07:55:34 +0700

    Step 1.7

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var d, h, m, m2 int
-	_, err := fmt.Scan(&d)
-	if err != nil {
-		return
-	}
-	if d > 360 {
-		return
-	}
-	// здесь ваш код
-	m = d * 2
-	h = m / 60
-	m2 = m % 60
-	fmt.Println("It is", h, "hours", m2, "minutes.")
+	var a int = 8
+	const b int = 10
+	a = a + b
+
+	fmt.Println(a)
 }
