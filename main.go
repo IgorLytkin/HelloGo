6460af0f6ad5147bd4f136690388adcb627ddf97
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-09 05:09:42 +0700

    Switch

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
-
-const (
-	a = iota + 1
-	_
-	b
-	c
-	d = c + 2
-	t
-	i
-	i2 = iota + 2
-)
+import "fmt"
 
 func main() {
-	fmt.Println(i2)
+	var n int
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		return
+	}
+	switch {
+	case n > 0:
+		fmt.Print("Число положительное")
+	case n < 0:
+		fmt.Print("Число отрицательное")
+	default:
+		fmt.Print("Ноль")
+	}
 }
