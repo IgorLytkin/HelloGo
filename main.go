fc22cbda4b824c43e076ad1ded8bb33ea2ced214
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-09 05:38:31 +0700

    Шаг 1.9

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var n int
+	var n, n1, n2, n3 int
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		return
 	}
-	switch {
-	case n > 0:
-		fmt.Print("Число положительное")
-	case n < 0:
-		fmt.Print("Число отрицательное")
-	default:
-		fmt.Print("Ноль")
+	n1 = n / 100
+	n2 = (n - n1*100) / 10
+	n3 = n - n1*100 - n2*10
+	if n1 == n2 || n1 == n3 || n2 == n3 {
+		fmt.Print("NO")
+	} else {
+		fmt.Print("YES")
 	}
 }
