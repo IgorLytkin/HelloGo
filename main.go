65e30a1c6369cde794a0d1bea7c6ab09a3a31944
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-09 05:55:29 +0700

    Шаг 1.9 задача 2

--- main.go
+++ main.go
@@ -16,15 +16,15 @@ func main() {
 			fmt.Print("Число должно быть меньше 10000 !")
 			return
 		}
-	case n > 1000:
+	case n >= 1000:
 		{
 			fmt.Print(n / 1000)
 		}
-	case n > 100:
+	case n >= 100:
 		{
 			fmt.Print(n / 100)
 		}
-	case n > 10:
+	case n >= 10:
 		{
 			fmt.Print(n / 10)
 		}
