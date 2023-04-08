8db17b10e2c530c3e41f45afcc0a798869de5df5
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-08 07:58:43 +0700

    Константы - iota

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import "fmt"
 
 func main() {
 	const (
-		Sunday    = 0
-		Monday    = 1
-		Tuesday   = 2
-		Wednesday = 3
-		Thursday  = 4
-		Friday    = 5
-		Saturday  = 6
+		Sunday = iota
+		Monday
+		Tuesday
+		Wednesday
+		Thursday
+		Friday
+		Saturday
 	)
+
 	fmt.Println(Sunday)   // вывод 0
 	fmt.Println(Saturday) // вывод 6
 
