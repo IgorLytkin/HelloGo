4d0c56d744f8f1cf6d4a344142843e6feb06bd61
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-04-13 03:54:05 +0700

    Шаг 1.10 Напишите программу, которая в последовательности чисел находит сумму двузначных чисел, кратных 8. Программа в первой строке получает на вход число n - количество чисел в последовательности, во второй строке -- n чисел, входящих в данную последовательность.
    Sample Input:
    
    5
    38 24 800 8 16
    Sample Output:
    
    40

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 )
 
 func main() {
-	var A, B int
-	fmt.Scan(&A)
-	fmt.Scan(&B)
+	var n, m int
+	fmt.Scan(&n)
 
 	var s int
 	s = 0
-	for i := A; i <= B; i++ {
-		s += i
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&m)
+		if (m%8 == 0) && (m >= 10 && m < 100) {
+			s += m
+		}
 	}
 	fmt.Println(s)
 }
